edb532a82ca8404f02097d15252996f636c1f839
LeeKien90 <lekien21090@gmail.com>
2023-03-13 08:57:45 +0700

    feature/#696/crud_category

--- book-store/src/main/java/ra/model/service/CategoryService.java
+++ book-store/src/main/java/ra/model/service/CategoryService.java
@@ -1,4 +1,10 @@
 package ra.model.service;
 
-public interface CategoryService {
+import ra.model.entity.Category;
+
+import java.util.List;
+
+public interface CategoryService<T,V> extends StoreBookService<T,V>{
+    List<Category> findByCategoryName(String name);
+    List<Category> sortCategoryByCategoryName(String direction);
 }
